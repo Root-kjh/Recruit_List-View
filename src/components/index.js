3bb89a0923c633dd644928ec4f0e93d1cb7258b8
Drk <wldnro3141@gmail.com>
2020-02-13 06:50:58 +0900

    userWrapper 추가

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,13 @@ import chooseContainers from "./chooseContainers/chooseContainers";
 import CompanyWrapper from "./companyWrapper/companyWrapper";
 import Company from "./company/company";
 import CompanyInfo from "./companyInfo/companyInfo";
+import userWrapper from "./userWrapper/userWrapper";
 export {
     Header,
     Footer,
     chooseContainers,
     CompanyWrapper,
     Company,
-    CompanyInfo
+    CompanyInfo,
+    userWrapper
 };
