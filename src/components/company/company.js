a7cbd1810ec18ee153b089e25630206ffc152d99
Drk <wldnro3141@gmail.com>
2020-02-12 18:00:34 +0900

    axios 연동중

--- src/components/company/company.js
+++ src/components/company/company.js
@@ -1,9 +1,30 @@
 import React from "react";
+import axios from "../../api/axios";
+class Company extends React.Component{
 
-const Company=(props)=>(
-    <div>
-        Company
-    </div>
-)
+    constructor(props){
+        super(props);
+        this.state={
+            Company : []
+        };
+    }
 
+    function=async text=>{
+        const response=await axios.get('/company/');
+
+        this.setState({
+            Company : response.data.result
+        });
+    }
+
+    getCompany();
+
+    render(){
+        return(
+            <div>
+            {this.responseOK && await this.response.data}
+            </div>
+        )
+    }
+}
 export default Company;
--- src/components/companyWrapper/companyWrapper.js
+++ src/components/companyWrapper/companyWrapper.js
@@ -3,16 +3,40 @@ import { Checkbox } from "react-md";
 import { Company } from "../";
 import { CompanyInfo } from "../";
 
-const CompanyWrapper=({isUser})=>(
-    <div>
-        <label>CompanyName</label><input name="companyName"/>
-        <label>채용 진행중</label><Checkbox/>
-        <label>최소 직원수</label><input name="employeesNum"/>
-        <label>최대 창립년도</label><input name="foundingYear"/>
-        <button>검색</button>
-        <Company isUser={isUser}/>
-        <CompanyInfo isUser={isUser}/>
-    </div>
-)
+class  CompanyWrapper extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.searchCompany=this.searchCompany.bind(this);
+        this.state={
+            CompanyName : "",
+            isRecruit : false,
+            employeesNum : new Date().getFullYear()
+          };
+    }
+
+    searchCompany() {
+        this.setState({
+            CompanyName : document.getElementsByName("companyName")[0].value,
+            isRecruit : document.getElementsByName("isRecruit")[0].value,
+            employeesNum : document.getElementsByName("employeesNum")[0].value,
+            foundingYear : document.getElementsByName("foundingYear")[0].value
+        });
+    }
+
+    render(){
+
+    return(
+        <div>
+            <label>CompanyName</label><input name="companyName" value={this.state.CompanyName}/>
+            <label>채용 진행중</label><Checkbox name="isRecruit" value={this.state.isRecruit}/>
+            <label>최소 직원수</label><input name="employeesNum" value={this.state.employeesNum}/>
+            <label>최대 창립년도</label><input name="foundingYear" value={this.state.foundingYear}/>
+            <button onClick={this.searchCompany}>검색</button>
+            <Company isUser={this.props.isUser}/>
+            <CompanyInfo isUser={this.props.isUser}/>
+        </div>
+    );
+    }
+}
 export default CompanyWrapper;
