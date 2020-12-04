1040ba283a316e29b725f2decbbbf6eb133d515f
kjh <wldnro3141@gmail.com>
2020-12-04 15:26:36 +0900

    update import uri

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Header from './header/header';
 import Footer from "./footer/footer";
 import Company from "./company/company";
-import Signin from "./signIn/Signin";
+import Signin from "./signIn/signin";
 import Signup from './signup/Signup';
 export {
     Header,
